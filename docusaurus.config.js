c4bbc1777ee1470afd48bf0d25f9bedb60943a64
Chris Anderson <jchris@gmail.com>
2023-07-01 04:38:29 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'use 🔥 Fireproof',
-  tagline: 'Live data hooks for React, accelerated by cryptographic proofs',
+  tagline: 'Live database hooks for React, accelerated by cryptographic proofs',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
